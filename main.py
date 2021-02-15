bb5b054e6ea04ddb60aae7e141e2741719aad7c1
Wanderson M. Pimenta <60605512+Wanderson-Magalhaes@users.noreply.github.com>
2021-02-15 13:36:04 -0300

    Bug FIX

---
+++ main.py
@@ -0,0 +1,92 @@
+# This Python file uses the following encoding: utf-8
+import sys
+import os
+import datetime
+
+from PySide2.QtGui import QGuiApplication
+from PySide2.QtQml import QQmlApplicationEngine
+from PySide2.QtCore import QObject, Slot, Signal, QTimer, QUrl
+
+class MainWindow(QObject):
+    def __init__(self):
+        QObject.__init__(self)
+
+        # QTimer - Run Timer
+        self.timer = QTimer()
+        self.timer.timeout.connect(lambda: self.setTime())
+        self.timer.start(1000)
+
+    # Signal Set Name
+    setName = Signal(str)
+
+    # Signal Set Data
+    printTime = Signal(str)
+
+    # Signal Visible
+    isVisible = Signal(bool)
+
+    # Open File To Text Edit
+    readText = Signal(str)
+
+    # Text String
+    textField = ""
+
+    # Open File
+    @Slot(str)
+    def openFile(self, filePath):
+        file = open(QUrl(filePath).toLocalFile(), encoding="utf-8")
+        text = file.read()
+        file.close()
+        print(text)
+        self.readText.emit(str(text))
+
+    # Read Text
+    @Slot(str)
+    def getTextField(self, text):
+        self.textField = text
+
+    # Write File
+    @Slot(str)
+    def writeFile(self, filePath):
+        file = open(QUrl(filePath).toLocalFile(), "w")
+        file.write(self.textField)
+        file.close()
+        print(self.textField)
+
+    # Show / Hide Rectangle
+    @Slot(bool)
+    def showHideRectangle(self, isChecked):
+        print("Is rectangle visible: ", isChecked)
+        self.isVisible.emit(isChecked)
+
+    # Set Timer Function
+    def setTime(self):
+        now = datetime.datetime.now()
+        formatDate = now.strftime("Now is %H:%M:%S %p of %Y/%m/%d")
+        print(formatDate)
+        self.printTime.emit(formatDate)
+
+    # Function Set Name To Label
+    @Slot(str)
+    def welcomeText(self, name):
+        self.setName.emit("Welcome, " + name)
+
+
+if __name__ == "__main__":
+    app = QGuiApplication(sys.argv)
+    engine = QQmlApplicationEngine()
+
+    # Get Context
+    main = MainWindow()
+    engine.rootContext().setContextProperty("backend", main)
+
+    # Set App Extra Info
+    app.setOrganizationName("Wanderson M. Pimenta")
+    app.setOrganizationDomain("N/A")
+
+    # Load QML File
+    engine.load(os.path.join(os.path.dirname(__file__), "qml/main.qml"))
+
+    if not engine.rootObjects():
+        sys.exit(-1)
+    sys.exit(app.exec_())
